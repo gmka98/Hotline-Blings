e0d0bd6996bd62cb95a041fe5c5e01f70046e111
gmk4 <gmka98@gmail.com>
2023-07-02 10:11:39 +0200

    Add project files.

---
+++ Hotline Blings/Phone.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Hotline_Blings
+{
+    partial class Phone
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Namelbl = new System.Windows.Forms.Label();
+            this.FirstNamelbl = new System.Windows.Forms.Label();
+            this.Phonelbl = new System.Windows.Forms.Label();
+            this.Emaillbl = new System.Windows.Forms.Label();
+            this.Adresslbl = new System.Windows.Forms.Label();
+            this.Searchlbl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.Categorylbl = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Citylbl = new System.Windows.Forms.Label();
+            this.Regionlbl = new System.Windows.Forms.Label();
+            this.PostCodelbl = new System.Windows.Forms.Label();
+            this.Countrylbl = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Namelbl
+            // 
+            this.Namelbl.AutoSize = true;
+            this.Namelbl.Location = new System.Drawing.Point(54, 36);
+            this.Namelbl.Name = "Namelbl";
+            this.Namelbl.Size = new System.Drawing.Size(35, 13);
+            this.Namelbl.TabIndex = 0;
+            this.Namelbl.Text = "Name";
+            // 
+            // FirstNamelbl
+            // 
+            this.FirstNamelbl.AutoSize = true;
+            this.FirstNamelbl.Location = new System.Drawing.Point(362, 43);
+            this.FirstNamelbl.Name = "FirstNamelbl";
+            this.FirstNamelbl.Size = new System.Drawing.Size(57, 13);
+            this.FirstNamelbl.TabIndex = 1;
+            this.FirstNamelbl.Text = "First Name";
+            // 
+            // Phonelbl
+            // 
+            this.Phonelbl.AutoSize = true;
+            this.Phonelbl.Location = new System.Drawing.Point(54, 74);
+            this.Phonelbl.Name = "Phonelbl";
+            this.Phonelbl.Size = new System.Drawing.Size(78, 13);
+            this.Phonelbl.TabIndex = 2;
+            this.Phonelbl.Text = "Phone Number";
+            // 
+            // Emaillbl
+            // 
+            this.Emaillbl.AutoSize = true;
+            this.Emaillbl.Location = new System.Drawing.Point(54, 105);
+            this.Emaillbl.Name = "Emaillbl";
+            this.Emaillbl.Size = new System.Drawing.Size(32, 13);
+            this.Emaillbl.TabIndex = 3;
+            this.Emaillbl.Text = "Email";
+            // 
+            // Adresslbl
+            // 
+            this.Adresslbl.AutoSize = true;
+            this.Adresslbl.Location = new System.Drawing.Point(54, 143);
+            this.Adresslbl.Name = "Adresslbl";
+            this.Adresslbl.Size = new System.Drawing.Size(45, 13);
+            this.Adresslbl.TabIndex = 4;
+            this.Adresslbl.Text = "Address";
+            // 
+            // Searchlbl
+            // 
+            this.Searchlbl.AutoSize = true;
+            this.Searchlbl.Location = new System.Drawing.Point(54, 350);
+            this.Searchlbl.Name = "Searchlbl";
+            this.Searchlbl.Size = new System.Drawing.Size(41, 13);
+            this.Searchlbl.TabIndex = 5;
+            this.Searchlbl.Text = "Search";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 36);
+            this.textBox1.MaximumSize = new System.Drawing.Size(200, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(150, 74);
+            this.textBox2.MaximumSize = new System.Drawing.Size(200, 0);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(457, 40);
+            this.textBox3.MaximumSize = new System.Drawing.Size(200, 0);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(173, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(150, 105);
+            this.textBox4.MaximumSize = new System.Drawing.Size(200, 0);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(173, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(150, 140);
+            this.textBox5.MaximumSize = new System.Drawing.Size(200, 0);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(173, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // Categorylbl
+            // 
+            this.Categorylbl.AutoSize = true;
+            this.Categorylbl.Location = new System.Drawing.Point(147, 288);
+            this.Categorylbl.Name = "Categorylbl";
+            this.Categorylbl.Size = new System.Drawing.Size(49, 13);
+            this.Categorylbl.TabIndex = 11;
+            this.Categorylbl.Text = "Category";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(293, 280);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 21);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // Citylbl
+            // 
+            this.Citylbl.AutoSize = true;
+            this.Citylbl.Location = new System.Drawing.Point(54, 183);
+            this.Citylbl.Name = "Citylbl";
+            this.Citylbl.Size = new System.Drawing.Size(24, 13);
+            this.Citylbl.TabIndex = 13;
+            this.Citylbl.Text = "City";
+            // 
+            // Regionlbl
+            // 
+            this.Regionlbl.AutoSize = true;
+            this.Regionlbl.Location = new System.Drawing.Point(362, 176);
+            this.Regionlbl.Name = "Regionlbl";
+            this.Regionlbl.Size = new System.Drawing.Size(41, 13);
+            this.Regionlbl.TabIndex = 14;
+            this.Regionlbl.Text = "Region";
+            // 
+            // PostCodelbl
+            // 
+            this.PostCodelbl.AutoSize = true;
+            this.PostCodelbl.Location = new System.Drawing.Point(54, 222);
+            this.PostCodelbl.Name = "PostCodelbl";
+            this.PostCodelbl.Size = new System.Drawing.Size(81, 13);
+            this.PostCodelbl.TabIndex = 15;
+            this.PostCodelbl.Text = "PostCode/ZIP :";
+            // 
+            // Countrylbl
+            // 
+            this.Countrylbl.AutoSize = true;
+            this.Countrylbl.Location = new System.Drawing.Point(362, 226);
+            this.Countrylbl.Name = "Countrylbl";
+            this.Countrylbl.Size = new System.Drawing.Size(43, 13);
+            this.Countrylbl.TabIndex = 16;
+            this.Countrylbl.Text = "Country";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(457, 173);
+            this.textBox6.MaximumSize = new System.Drawing.Size(200, 0);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(173, 20);
+            this.textBox6.TabIndex = 17;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(457, 219);
+            this.textBox7.MaximumSize = new System.Drawing.Size(200, 0);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(173, 20);
+            this.textBox7.TabIndex = 18;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(150, 176);
+            this.textBox8.MaximumSize = new System.Drawing.Size(200, 0);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(173, 20);
+            this.textBox8.TabIndex = 19;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(150, 215);
+            this.textBox9.MaximumSize = new System.Drawing.Size(200, 0);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(173, 20);
+            this.textBox9.TabIndex = 20;
+            // 
+            // Phone
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1065, 495);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.Countrylbl);
+            this.Controls.Add(this.PostCodelbl);
+            this.Controls.Add(this.Regionlbl);
+            this.Controls.Add(this.Citylbl);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Categorylbl);
+            this.Controls.Add(this.Searchlbl);
+            this.Controls.Add(this.Adresslbl);
+            this.Controls.Add(this.Emaillbl);
+            this.Controls.Add(this.Phonelbl);
+            this.Controls.Add(this.FirstNamelbl);
+            this.Controls.Add(this.Namelbl);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Phone";
+            this.Text = "Phone";
+            this.Load += new System.EventHandler(this.Phone_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Namelbl;
+        private System.Windows.Forms.Label FirstNamelbl;
+        private System.Windows.Forms.Label Phonelbl;
+        private System.Windows.Forms.Label Emaillbl;
+        private System.Windows.Forms.Label Adresslbl;
+        private System.Windows.Forms.Label Searchlbl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label Categorylbl;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label Citylbl;
+        private System.Windows.Forms.Label Regionlbl;
+        private System.Windows.Forms.Label PostCodelbl;
+        private System.Windows.Forms.Label Countrylbl;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+    }
+}
